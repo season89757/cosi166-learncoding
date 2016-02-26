e8c090d216265634ca76dfd67ba72cdd0cb47651
jaosn <hanzhenyugg@gmail.com>
2016-02-26 14:33:24 -0500

    books extract from the GoodReads API

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require_relative "../goodapi"
+
 User.create(username: "liuchao", display_name: "Chao Liu", password: "1", email: "lc89757@brandeis.edu")
 User.create(username: "hanzhenyu", display_name: "Zhenyu Han", password: "1", email: "hanzhenyu@brandeis.edu")
 Book.create(title: "Agile Web Development with Rails", author: "Sam Ruby")
 Book.create(title: "Don't Make Me Think", author: "Steve Krug")
+
+result = GoodR.new(20).results
+result.each do |single|
+  Book.create(title: single.best_book.title, author:single.best_book.author.name)
+end
---
+++ goodapi.rb
@@ -0,0 +1,46 @@
+require 'goodreads'
+
+class GoodR
+  attr_accessor :results
+  def initialize(num)
+    @key = "vBvspX3h3XcI5YboURDBg"
+    @secret = "dMmUn90jCCmBEKvnjuhpvQ7h53dOY6PSt7DX7Gg9Vk"
+    @client = Goodreads.new(:api_key => @key)
+    @max = 2000
+    @results = []
+    get(num)
+  end
+
+  def get(num)
+    if check?(num) then search(num) else search(@max) end
+  end
+
+  def check?(num)
+    if num <= 2000 then true else false end
+  end
+
+  def search(num)
+    page = num / 20 - 1
+    for n in 0..page do
+      r = @client.search_books('computer science',:page => page.to_s)
+      r.results.work.each do |single|
+        @results << single
+      end
+    end
+  end
+end
+
+
+
+# @key = "vBvspX3h3XcI5YboURDBg"
+# @secret = "dMmUn90jCCmBEKvnjuhpvQ7h53dOY6PSt7DX7Gg9Vk"
+# @final = []
+#
+# #client = Goodreads::Client.new(:api_key => 'KEY', :api_secret => 'SECRET')
+# client = Goodreads.new(:api_key => @key) # short version
+# for n in 0..4 do
+#   search = client.search_books('computer science',:page => n.to_s)
+#   search.results.work.each do |single|
+#     @final << single
+#   end
+# end
