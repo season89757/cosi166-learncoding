5317c41d3cf41f57a88056f265764aaecfc90bd6
season89757 <lc89757@brandeis.edu>
2016-03-01 22:21:04 -0500

    revise the controller for result v4

--- app/controllers/imdb_controller.rb
+++ app/controllers/imdb_controller.rb
@@ -11,7 +11,9 @@ class ImdbController < ApplicationController
   def result
     @book_name = 'Java 7 Programming'
     @book_rating = 4.5
-    @book_description='this is the description of the book'
+    @book_description='this is the description of the book,in this book you will learn how to use java and java script to design web. Java language redirects here. For the natural language from the Indonesian island of Java, see Javanese language.
+    This article is about a programming language. For the software package downloaded from java.com, see Java SE.
+    Not to be confused with JavaScript.'
   end
 
   def detail
