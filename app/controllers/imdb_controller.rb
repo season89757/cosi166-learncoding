964071c0306167ecb384e2f7cd1afe2397cc4610
jaosn <hanzhenyugg@gmail.com>
2016-03-03 15:59:29 -0500

    delete cancel botton

--- app/controllers/imdb_controller.rb
+++ app/controllers/imdb_controller.rb
@@ -33,7 +33,7 @@ class ImdbController < ApplicationController
         if user_exists || email_exists
           redirect_to imdb_register_path(:error => "User or Email already exists")
         else
-          @user = User.create(username: params[:username], password: params[:password], email: params[:email], display_name: params[:display_name], admin: false)
+          @user = User.create(username: params[:username], password: params[:password], email: params[:email], display_name: params[:display_name])
           redirect_to imdb_index_path(:user_id => @user)
         end
       end
@@ -47,7 +47,8 @@ class ImdbController < ApplicationController
     @tag3 = 'tag3'
     @book_name = 'Java 7 Programming'
     @book_rating = 4.5
-    @book_description='this is the description of the book,in this book you will learn how to use java and java script to design web. Java language redirects here. For the natural language from the Indonesian island of Java, see Javanese language.
+    @book_description='this is the description of the book,in this book you will learn how to use
+    java and java script to design web. Java language redirects here. For the natural language from the Indonesian island of Java, see Javanese language.
     This article is about a programming language. For the software package downloaded from java.com, see Java SE.
     Not to be confused with JavaScript.'
     @firstpost = 'first post'
