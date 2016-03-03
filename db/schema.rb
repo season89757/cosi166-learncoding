c09788a47944dc45130f970babc4d5e92ee6ce3c
anboi1225 <anboi1225@gmail.com>
2016-03-03 12:56:45 -0500

    session_controller_test

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20160302144646) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "books", force: :cascade do |t|
     t.string   "title"
     t.integer  "ISBN"
--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,5 +1,23 @@
-require 'test_helper'
+require_relative '../test_helper'
+require 'minitest/autorun'
+require 'rails/all'
+#require_relative '../../app/controllers/sessions_controller'
 
 class SessionsControllerTest < ActionController::TestCase
+  def setup
+    @sessions_controller = SessionsController.new
+    @test_user = User.create(username: "faker",
+                             email: "test@test.com",
+                             password: "password")
+  end
+
+  test "create successfully" do
+    assert_not_nil(@sessions_controller)
+  end
+
+  test "the create method works" do
+    assert(@sessions_controller.create_test("faker",@test_user.password))
+    assert_not(@sessions_controller.create_test("not exist user", "whatever"))
+  end
 
 end
