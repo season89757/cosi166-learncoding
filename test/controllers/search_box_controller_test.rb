224cec7e55f7c584e3e731eadf0eefb8a5c6d52d
breyerjs <breyer.js@gmail.com>
2016-03-03 16:02:10 -0500

    Adding some more search features

--- test/controllers/search_box_controller_test.rb
+++ test/controllers/search_box_controller_test.rb
@@ -4,6 +4,8 @@ class SearchBoxControllerTest < ActionController::TestCase
     def setup
         @one_term = "python"
         @two_terms = "python ruby"
+        @two_terms_with_stopwords = "by the python of ruby because for"
+        @two_terms_with_commas = "python, ruby"
         @python_book = Book.create(title:      "Python for Dummies",
                                     ISBN:       42,
                                     author:     "Hanz",
@@ -18,6 +20,8 @@ class SearchBoxControllerTest < ActionController::TestCase
     test "searches for one term" do 
         get :search_results, terms: @one_term
         assert_includes(assigns('results'), @python_book)
+        #only removes duplicates properly
+        assert_equal(1, assigns('results').length)
     end
 
     test "searches for multiple terms" do
@@ -25,5 +29,16 @@ class SearchBoxControllerTest < ActionController::TestCase
         assert_includes(assigns('results'), @python_book)
         assert_includes(assigns('results'), @ruby_book)
     end
+
+    test "search removes stopwords" do
+        get :search_results, terms: @two_terms_with_stopwords
+        assert_equal ["python", "ruby"], assigns('terms')        
+    end
+
+    test "search removes commas" do 
+        get :search_results, terms: @two_terms_with_commas
+        assert_equal ["python", "ruby"], assigns('terms')    
+    end
+
 
 end
