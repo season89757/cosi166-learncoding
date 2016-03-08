968d4c7bfc4429f0c53d9e97ea445885d1cfd3df
jaosn <hanzhenyugg@gmail.com>
2016-03-08 12:46:13 -0500

    add logout

--- config/routes.rb
+++ config/routes.rb
@@ -36,6 +36,8 @@ Rails.application.routes.draw do
 
   get 'imdb/detail'
 
+  get 'imdb/logout'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
