3f1ef1ab6fa112bf40a529cc64b2e323fb0b46ef
breyerjs <breyer.js@gmail.com>
2016-03-30 15:49:03 -0400

    work on forums

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   get 'search_box/search_results'
 
+  get 'search_box/book_detail'
+
   resources :books
   resources :users
 
@@ -22,6 +24,7 @@ Rails.application.routes.draw do
   get 'imdb/logout'
 
   get 'imdb/searchresult'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
