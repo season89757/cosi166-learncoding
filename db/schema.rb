0446392e8b63c60ff0f43bc15350063cfcb98ad8
breyerjs <breyer.js@gmail.com>
2016-03-31 16:08:03 -0400

    compatability with current migrations

--- db/schema.rb
+++ db/schema.rb
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20160331160337) do
     t.integer  "book_id"
     t.text     "title"
     t.text     "body"
+    t.integer  "like"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
