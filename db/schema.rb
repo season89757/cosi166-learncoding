5a5b377cc77ea737fca11992589996318ab540a5
jaosn <hanzhenyugg@gmail.com>
2016-03-31 15:31:11 -0400

    push no noghing

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160325193507) do
+ActiveRecord::Schema.define(version: 20160325200451) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -35,7 +35,6 @@ ActiveRecord::Schema.define(version: 20160325193507) do
     t.integer  "book_id"
     t.text     "title"
     t.text     "body"
-    t.integer  "like"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -45,6 +44,13 @@ ActiveRecord::Schema.define(version: 20160325193507) do
 
   create_table "domains", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "comment_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
