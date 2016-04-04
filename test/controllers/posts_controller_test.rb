100372c2ce4cfe9b044e3f172bbdbce84623d79a
breyerjs <breyer.js@gmail.com>
2016-04-04 13:03:50 -0400

    some tests for the forums

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -2,48 +2,36 @@ require 'test_helper'
 
 class PostsControllerTest < ActionController::TestCase
   setup do
-    @post = posts(:one)
+    @forum_one  = Forum.create(name: "Linguistic Theory")
+    @post_one_title = "Does syntax exist separate from semantics?"
+    @post_one_content = "Colorless green ideas sleep furiously."
+    @post_one = Post.create(title: @post_one_title, 
+                            content: @post_one_content,
+                            forum_id: @forum_one.id)
+    @postreply_one = Postreply.create(content: "Gnome Chomp Ski", 
+                                      post_id: @post_one.id)
+    @postreply_two = Postreply.create(content: "Second post", 
+                                      post_id: @post_one.id)
+
   end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:posts)
+  test "should save title and content of post" do
+    assert_equal @post_one.title, @post_one_title
+    assert_equal @post_one.content, @post_one_content
   end
 
-  # test "should get new" do
-  #   get :new
-  #   assert_response :success
-  # end
-
-  # test "should create post" do
-  #   assert_difference('Post.count') do
-  #     post :create, post: {  }
-  #   end
-
-  #   assert_redirected_to post_path(assigns(:post))
-  # end
-
-  # test "should show post" do
-  #   get :show, id: @post
-  #   assert_response :success
-  # end
-
-  test "should get edit" do
-    get :edit, id: @post
-    assert_response :success
+  # might logically go in the forums tests, but I think it makes sense here
+  test "forum should have all posts" do
+    assert_equal @forum_one.posts.length, 1
   end
 
-  # test "should update post" do
-  #   patch :update, id: @post, post: {  }
-  #   assert_redirected_to post_path(assigns(:post))
-  # end
-
-  test "should destroy post" do
-    assert_difference('Post.count', -1) do
-      delete :destroy, id: @post
-    end
-
-    assert_redirected_to posts_path
+  test "posts should have all postreplies" do 
+    assert_equal @post_one.postreplies.length, 2
   end
+
+  test "postreplies are loaded in order" do
+    assert_equal @post_one.postreplies[0], @postreply_one
+    assert_equal @post_one.postreplies[1], @postreply_two
+  end
+
 end
