e0346c6c9c2529e3e9f03c63a728731e5d4df9ee
anboi1225 <anboi1225@gmail.com>
2016-04-10 01:22:13 -0400

    heroku test

---
+++ lib/image_processing.rb
@@ -0,0 +1,4 @@
+require 'base64'
+image = open('/home/boyang/Dropbox/cosi166b_ban/cosi166-learncoding/app/assets/images/imdb_logo25.png') { |f| f.read }
+test1 = Base64.encode64(image)
+plain = Base64.decode64(test1)
