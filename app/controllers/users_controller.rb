0b9a2e3dc3bd06d709c90a42c7cf5db72c7a23eb
season89757 <lc89757@brandeis.edu>
2016-05-04 16:07:44 -0400

    get

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,8 @@ class UsersController < ApplicationController
   def profile
     @user = User.find_by(id:session[:imdb_user_id])
     @photo = Userphoto.find_by(userid:session[:imdb_user_id])
-    # query_string = params[:terms]
+    @message = Message.where("receiver = ?",session[:imdb_user_id])
+      # query_string = params[:terms]
     # tag = params[:tag]
     # #@results = Book.run_search(query_string, tag= tag)
     # redirect_to search_box_search_results_path
