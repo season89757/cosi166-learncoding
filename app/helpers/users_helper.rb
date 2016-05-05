f488bac95877ca28c692507ee9d8b9fced1dc62f
season89757 <lc89757@brandeis.edu>
2016-05-05 15:01:56 -0400

    add the favourites in profile page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,6 +8,11 @@ module UsersHelper
     end
   end
 
+  def find_book(book_id)
+    book = Book.find_by(id: book_id)
+    return book
+  end
+
   def find_userphoto(user_id)
     userphoto = Userphoto.find_by(userid: user_id)
     if userphoto
